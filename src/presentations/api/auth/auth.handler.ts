50150dbc350e9f1ae91be7a34137520100bdac70
Muhammad Nizar <mnizarzr@gmail.com>
2023-12-20 10:39:41 +0700

    add user info to login (not just token)

--- src/presentations/api/auth/auth.handler.ts
+++ src/presentations/api/auth/auth.handler.ts
@@ -2,6 +2,10 @@ import { Response, Request } from 'express'
 import { DependencyContainer } from 'tsyringe'
 import LoginUserUseCase from '@/applications/use-cases/auth/login.use-case'
 import LogoutUserUseCase from '@/applications/use-cases/auth/logout.use-case'
+import GetSingleUserUseCase from '@/applications/use-cases/users/get-user.use-case'
+import { decodeToken } from '@/infrastructures/security/jwt-manager'
+import { Jwt } from 'jsonwebtoken'
+import { roles } from '../../../infrastructures/data-sources/mysql/schema/users'
 
 class AuthHandler {
   private container: DependencyContainer
@@ -14,9 +18,12 @@ class AuthHandler {
     const { email, password } = req.body
 
     const loginUseCase = this.container.resolve(LoginUserUseCase)
+    const getUserUseCase = this.container.resolve(GetSingleUserUseCase)
     const authTokens = await loginUseCase.execute({ email, password })
+    const decoded = (decodeToken(authTokens.accessToken) as Jwt).payload
+    const user = await getUserUseCase.execute(decoded.sub! as string)
 
-    res.json(authTokens)
+    res.json({ user: { ...user, role: decoded.role }, token: authTokens.accessToken })
   }
 
   postUserLogout = async (req: Request, res: Response) => {
